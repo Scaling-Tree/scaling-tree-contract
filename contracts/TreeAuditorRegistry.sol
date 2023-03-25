cd611cf5dcd23fa1608543de8de2195737820410
Warun S <warunsinx@gmail.com>
2023-03-25 10:59:10 +0700

    implemented TreeAuditorRegistry

---
+++ contracts/TreeAuditorRegistry.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "./interfaces/ITreeAuditorRegistry.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TreeAuditorRegistry is ITreeAuditorRegistry, Ownable {
+    mapping(address => Auditor) public auditors;
+
+    function addAuditor(address auditor, uint score) public onlyOwner {
+        auditors[auditor].isAuditor = true;
+        auditors[auditor].score = score;
+        emit AuditorAdded(auditor, score);
+    }
+
+    function removeAuditor(address auditor) public onlyOwner {
+        auditors[auditor].isAuditor = false;
+        emit AuditorRemoved(auditor);
+    }
+
+    function setAuditorScore(address auditor, uint256 score) public onlyOwner {
+        auditors[auditor].score = score;
+        emit AuditorScoreUpdated(auditor, score);
+    }
+
+    function isAuditor(address auditor) public view returns (bool) {
+        return auditors[auditor].isAuditor;
+    }
+
+    function getAuditorScore(address auditor) public view returns (uint256) {
+        return auditors[auditor].score;
+    }
+}
---
+++ contracts/interfaces/ITreeAuditorRegistry.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface ITreeAuditorRegistry {
+    struct Auditor {
+        bool isAuditor;
+        uint256 score;
+    }
+
+    event AuditorAdded(address indexed auditor_, uint score_);
+    event AuditorRemoved(address indexed auditor_);
+    event AuditorScoreUpdated(address indexed auditor_, uint256 score_);
+
+    function addAuditor(address auditor_, uint score_) external;
+
+    function removeAuditor(address auditor) external;
+
+    function setAuditorScore(address auditor, uint256 score) external;
+
+    function isAuditor(address auditor) external view returns (bool);
+
+    function getAuditorScore(address auditor) external view returns (uint256);
+}
