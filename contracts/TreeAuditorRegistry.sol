27299d0250cb2bbc1a04003d3c07e7d43182b6de
Sainy <sainy@mytrinket.xyz>
2023-03-25 12:14:30 +0700

    Fixed version

--- contracts/TreeAuditorRegistry.sol
+++ contracts/TreeAuditorRegistry.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity 0.8.18;
 
 import "./interfaces/ITreeAuditorRegistry.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
