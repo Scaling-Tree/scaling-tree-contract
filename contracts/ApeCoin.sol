3f9c402a89af11a4d053d9787c0af2f73d3c8854
Warun S <warunsinx@gmail.com>
2023-03-25 10:34:22 +0700

    make erc20 voteable

--- contracts/ApeCoin.sol
+++ contracts/ApeCoin.sol
@@ -2,9 +2,35 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract ApeCoin is ERC20 {
-    constructor() ERC20("ApeCoin", "APE") {
+contract ApeCoin is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20("ApeCoin", "APE") ERC20Permit("ApeCoin") {
         _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+    function _burn(
+        address account,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._burn(account, amount);
     }
 }
